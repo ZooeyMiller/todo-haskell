b722a6ba49ce43961a91c50996542dd54754ae7c
zooeymiller <zooeyxmiller@gmail.com>
2018-10-17 08:24:48 +0100

    add todo logic

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,28 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+type Todo = (Int, (String, Bool))
+
+type Todos = [Todo]
+
+indexed :: [a] -> [(Int, a)]
+indexed xs = go xs [] 1
+    where go [] ys _     = ys
+          go (x:xs) ys i = go xs (ys ++ [(i, x)]) (i + 1)
+
+getTodoInfo :: Todos -> [(String, Bool)]
+getTodoInfo = map snd
+
+reIndex :: Todos -> Todos
+reIndex = indexed . getTodoInfo
+
+addTodo :: Todos -> String -> Todos
+addTodo ts t = indexed $ getTodoInfo ts ++ [(t, False)]
+
+removeTodo :: Todos -> Int -> Todos
+removeTodo ts i = reIndex $ filter ((/=i) . fst) ts
+
+toggleTodo :: Todos -> Int -> Todos
+toggleTodo ts i = map update ts
+    where update td@(j, (t, b))
+            | j == i = (j, (t, not b))
+            | otherwise = td
